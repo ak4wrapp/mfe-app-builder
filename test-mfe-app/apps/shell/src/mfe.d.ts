8b48ee670d56eeb3cc149277e6b062c1446107cc
Amandeep Kirar <ak4wrapp@gmail.com>
2026-01-12 12:43:25 -0500

    test-mfe-app

---
+++ test-mfe-app/apps/shell/src/mfe.d.ts
@@ -0,0 +1,11 @@
+declare module "mfe1/App" {
+  import { ComponentType } from "react";
+  const mfe1_App: ComponentType;
+  export default mfe1_App;
+}
+
+declare module "mfe2/App" {
+  import { ComponentType } from "react";
+  const mfe2_App: ComponentType;
+  export default mfe2_App;
+}
