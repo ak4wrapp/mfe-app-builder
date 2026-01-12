df1d153cb07d38c51d50901bf8f61fc18bbfa0b2
Amandeep Kirar <ak4wrapp@gmail.com>
2026-01-12 10:10:25 -0500

    shell fix

--- my-first-mfe/apps/shell/vite.config.ts
+++ my-first-mfe/apps/shell/vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  console.log("Vite mode:", mode);
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log("Loaded env variables:", env);
+
   return {
     plugins: [
       react(),
       federation({
-        name: 'shell',
+        name: "shell",
         remotes: {
           mfe1: `${env.VITE_MFE1_URL}`,
-          mfe2: `${env.VITE_MFE2_URL}`
+          mfe2: `${env.VITE_MFE2_URL}`,
         },
-        shared: ['react', 'react-dom']
-      })
-    ]
-  }
-})
-
+        shared: ["react", "react-dom"],
+      }),
+    ],
+  };
+});
